250b1399d96635c3a339fe13cb77bf4f76667b58
iermakovets <vania.ermakovets@gmail.com>
2016-09-23 12:42:47 +0300

    Move authentication buttons to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def authentication_buttons
+    if user_signed_in?
+      content_tag :li, class: 'nav-item' do
+        link_to "Sign out", destroy_user_session_path,
+                method: :delete, class: 'nav-link'
+      end
+    else
+      content = content_tag :li, class: 'nav-item' do
+        link_to "Sign up", new_user_registration_path, class: 'nav-link'
+      end
+      content << content_tag(:li, class: 'nav-item') do
+        link_to "Sign in", new_user_session_path, class: 'nav-link'
+      end
+    end
+  end
 end
