a393651bb3e52e7d68e7c445e3ee856ed6751718
Eugene Traytyak <evgen.traytyak@gmail.com>
2016-10-04 21:34:23 +0300

    Create base service

---
+++ app/services/games/base_service.rb
@@ -0,0 +1,29 @@
+module Games
+  class BaseService
+    attr_reader :game
+
+    def initialize(game)
+      @game = game
+    end
+
+    def start
+      game.run_validation!
+
+      game.update(validations_count: validations_list.count)
+
+      validations_list.each do |validation_name|
+        ValidationWorker.perform_async(game.id, validation_name)
+      end
+    end
+
+    private
+
+    def validations_list
+      [
+        'Validations::BlacklistValidationService',
+        'Validations::SufficientBetAmountValidationService'
+      ]
+    end
+
+  end
+end
