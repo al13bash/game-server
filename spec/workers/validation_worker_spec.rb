ff16fc19bc500f6b3cb3f93383a225edd2af2bbb
iermakovets <vania.ermakovets@gmail.com>
2016-10-06 15:02:19 +0300

    Test execution of workers

--- spec/workers/validation_worker_spec.rb
+++ spec/workers/validation_worker_spec.rb
@@ -1,12 +1,24 @@
 require 'rails_helper'
 
 describe ValidationWorker do
-  subject(:job) { described_class.perform_async }
+  subject(:job) { described_class.perform_async(game.id, service_name) }
+
+  let(:game) { Fabricate(:game) }
+  let(:service_name) { 'Validations::BlacklistValidationService' }
 
   it { expect(described_class).to be_processed_in :validations }
   it { expect(described_class).to be_retryable 3 }
 
   it 'enqueue a job' do
     expect { job }.to change(described_class.jobs, :size).by(1)
+  end
+
+  it 'executes perform' do
+    validation_service = double(service_name)
+    allow(validation_service).to receive(:validate).and_return(no_args)
+
+    expect(service_name.constantize).to receive(:new).with(game.id) { validation_service }
+
+    Sidekiq::Testing.inline! { job }
   end
 end
