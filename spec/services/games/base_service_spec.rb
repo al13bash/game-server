df1437cff4c045b3e172fee78fdea1d719dad5e8
Mikhail Dyomin <mikhaildyomin@gmail.com>
2016-10-07 10:40:29 +0300

    Validate Games::BaseService

--- spec/services/games/base_service_spec.rb
+++ spec/services/games/base_service_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Games::BaseService do
+  let(:user) { create :user }
+  let(:account) { create :account, user_id: user.id }
+
+  context 'initialized with game' do
+    let(:action) { described_class.new(game).start }
+    let!(:game) { create :game, user_id: user.id, account_id: account.id }
+
+    it 'changes game status to in_validation' do
+      expect { action }
+        .to change { game.reload.status }
+        .from('pending')
+        .to('in_validation')
+    end
+
+    it 'changes validations_count' do
+      expect { action }
+        .to change { game.reload.validations_count }
+        .from(nil)
+    end
+  end
 end
