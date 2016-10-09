0d47d8715602f64311a6b027995132be3caa9f34
Egor Vorobiev <vorobievegoralexandrovich@gmail.com>
2016-10-10 02:51:18 +0300

    Fix sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,5 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redistogo:575b78ca1e7be9c87c5461d456429ced@porgy.redistogo.com:9983/' }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redistogo:575b78ca1e7be9c87c5461d456429ced@porgy.redistogo.com:9983/' }
+if Rails.env.production?
+  Sidekiq.configure_client do |config|
+    config.redis = { url: 'redis://redistogo:575b78ca1e7be9c87c5461d456429ced@porgy.redistogo.com:9983/' }
+  end
 end
