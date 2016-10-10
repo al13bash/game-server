9b56882883e556dc6a22b48651905cfb01948482
Egor Vorobiev <vorobievegoralexandrovich@gmail.com>
2016-10-10 03:30:37 +0300

    Misha please, send a PR next time. Thnx

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,11 @@
 if Rails.env.production?
+  url = 'redis://redistogo:575b78ca1e7be9c87c5461d456429ced@porgy.redistogo.com:9983/'
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: url }
+  end
+
   Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://redistogo:575b78ca1e7be9c87c5461d456429ced@porgy.redistogo.com:9983/' }
+    config.redis = { url: url }
   end
 end
