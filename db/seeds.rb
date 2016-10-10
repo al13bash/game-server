6ad723fb12ab61111f3f259d2c9e3204f67371ff
Eugene Traytyak <evgen.traytyak@gmail.com>
2016-10-10 13:12:13 +0300

    Add app_errors seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@ User.create!(
   password: 'foobar13',
   username: 'adminushka'
 ) if User.count.zero?
+
+errors_messages = [
+  {
+    kind: :min_bet_amount_is_not_reached,
+    message: "Min bet amount is not reached"
+  },
+  {
+    kind: :max_bet_amount_exceeded,
+    message: "Max bet amount exceeded"
+  },
+  {
+    kind: :insufficient_funds_in_account,
+    message: "Insufficient funds in the account"
+  },
+  {
+    kind: :user_in_blacklist,
+    message: "User in blacklist"
+  },
+  {
+    kind: :validation_failed,
+    message: "Validation failed"
+  }
+]
+
+errors_messages.each{ |error| AppError.create(error) }
